18f7bf1eed12927b58af34ed43796250f97ba296
David BOUCHÉ <david@clicproxy.com>
2015-05-07 19:37:56 +0200

    Rajout les éléments Route, ...

--- src/Clicproxy/PolleetBundle/Controller/DefaultController.php
+++ src/Clicproxy/PolleetBundle/Controller/DefaultController.php
@@ -7,6 +7,9 @@ use Clicproxy\PolleetBundle\Entity\Person;
 use Clicproxy\PolleetBundle\Entity\Poll;
 use Clicproxy\PolleetBundle\Entity\PollAnswer;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class DefaultController extends Controller
 {
